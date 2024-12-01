027ee99a3633942df2af9f21b15172093cc6d923
Eric Huang <erichuang@mac.mynetworksettings.com>
2024-12-01 01:47:39 -0500

    Added differenitations to run different scripts with different parameters

--- get.filename.sh
+++ get.filename.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 URL=$1
+FILE_TYPE=$2
+
+
 
 # Use yt-dlp to get the filename
-FILENAME=$(yt-dlp --get-filename -o "%(title)s.%(ext)s" "$URL")
+if [ "$FILE_TYPE" == "video" ]; then
+FILENAME=$(yt-dlp -x --get-filename -o "%(title)s.%(ext)s" "$URL")
+fi
+
+if [ "$FILE_TYPE" == "audio" ]; then
+FILENAME=$(yt-dlp -x --get-filename -o "%(title)s.%(ext)s" "$URL")
+FILENAME="${FILENAME%.*}.mp3"
+fi
 
 # Output only the filename
 echo "$FILENAME"
